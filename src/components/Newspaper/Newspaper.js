e07e545e1e70fc91a4c3a40d4f199acab0d215c1
Mohammad Zakir Hossain <engineer.zakir.hossain@gmail.com>
2024-01-19 22:38:51 +0200

    material-ui

---
+++ src/components/Newspaper/Newspaper.js
@@ -0,0 +1,29 @@
+import { Box, Grid } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import News from '../News/News';
+
+const Newspaper = () => {
+    const [articles, setArticles] = useState([]);
+
+    useEffect(() => {
+        fetch('https://newsapi.org/v2/everything?q=bitcoin&apiKey=e19659c585ee4f93a0c61fc918ab1716')
+            .then(res => res.json())
+            .then(data => setArticles(data.articles));
+    }, [])
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+          {
+            articles.map(article => 
+                <Grid item xs={2} sm={4} md={4} >
+                    <News article = {article}></News>
+                </Grid>
+            )
+          }
+        </Grid>
+      </Box>
+    );
+};
+
+export default Newspaper;
